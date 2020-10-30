97d6f671d8171bef9cee96e82d941da99ed56274
Napoleon994 <71061099+Napoleon994@users.noreply.github.com>
2020-10-30 19:00:19 +0530

    Add an introduction to the game

--- DragonCave.py
+++ DragonCave.py
@@ -1,9 +1,17 @@
 import random
 import time
 
+def displayIntro():
+	print('You are in a land full of dragons. In front of you,')
+	print('you see two caves. In one cave, the dragon is friendly')
+	print('and will share his treasure with you. The other dragon')
+	print('is greedy and hungry, and will eat you on sight.')
+	print()
+
 playAgain = 'yes'
 
 while playAgain == 'yes' or playAgain == 'y':
+	displayIntro()
 	caveNumber
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
