a4e6ed85b1120e111d87e47d09b9a658b9266d61
Napoleon994 <71061099+Napoleon994@users.noreply.github.com>
2020-10-30 19:17:43 +0530

    Add main game logic
    This function checks whether the cave the player has decided to enter has a friendly dragon and decides the win or loss based on that.

--- DragonCave.py
+++ DragonCave.py
@@ -15,12 +15,30 @@ def chooseCave():
 		cave = input()
 	return cave
 
+def checkCave(chosenCave):
+	print('You approach the cave...')
+	time.sleep(2)
+	print('Ot is dark and spooky...')
+	time.sleep(2)
+	print('A large dragon jumps out in front of you! he opens his jaws and...')
+	print()
+	time.sleep(2)
+	
+	friendlyCave = random.randint(1, 2)
+	
+	if chosenCave == str(friendlyCave):
+		print('Gives you his treasure!')
+	else:
+		print('Gobbles you down in one bite!')
+
 playAgain = 'yes'
 
 while playAgain == 'yes' or playAgain == 'y':
 	displayIntro()
 	
 	caveNumber = chooseCave()
+	
+	checkCave(caveNumber)
 	
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
